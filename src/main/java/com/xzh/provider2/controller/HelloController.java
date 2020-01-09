a3665f4e384ef7f781c7bc8f243150a6d16942f4
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-01-09 14:55:44 +0800

    add JDK HttpClient

--- src/main/java/com/xzh/provider2/controller/HelloController.java
+++ src/main/java/com/xzh/provider2/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.xzh.provider2.controller;
 
+import com.xzh.provider2.dto.UserRequestDto;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -10,16 +11,16 @@ import org.springframework.web.bind.annotation.*;
  **/
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    public String index(@RequestParam String name) throws InterruptedException {
+    @GetMapping("/hello")
+    public String index(@RequestParam(required = false) String name) throws InterruptedException {
 //        Thread.sleep(8000);
-        return "这是服务提供者2，参数："+name;
+        return "这是服务提供者2，参数：" + name;
     }
 
     @PostMapping("/hello")
     @ResponseBody
-    public String ribbonPost(@RequestBody String name) {
-        return "这是服务提供者2，post请求,参数：" + name;
+    public String ribbonPost(@RequestBody UserRequestDto userRequestDto) {
+        return "这是服务提供者2，post请求,参数：" + userRequestDto.getName();
     }
 
 }
---
+++ src/main/java/com/xzh/provider2/controller/JdkHttpClinetController.java
@@ -0,0 +1,35 @@
+package com.xzh.provider2.controller;
+
+import com.xzh.provider2.dto.UserRequestDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author ：xzh
+ * @date ：Created in 2020-01-09 11:17
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@RestController
+@RequestMapping("/jdk_http_client")
+public class JdkHttpClinetController {
+
+    @GetMapping("/getTest")
+    public String getTest(@RequestParam(required = false) String name) {
+        return "这是服务提供者2，getTest -- 参数 name ：" + name;
+    }
+
+    @PostMapping(value = "/postJsonTest", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public String postJsonTest(@RequestBody UserRequestDto userRequestDto) {
+        return "这是服务提供者2，postJsonTest,参数 name：" + userRequestDto.getName() + ", age :" + userRequestDto.getAge();
+    }
+
+    @PostMapping(value = "/postFormTest", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    public String postFormTest(@RequestBody UserRequestDto userRequestDto) {
+        System.out.println("================");
+        return "这是服务提供者2，postFormTest,参数 name：" + userRequestDto.getName() + ", age :" + userRequestDto.getAge();
+    }
+}
